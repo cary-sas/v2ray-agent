6d28130df5a6c71e0c47ba1d7bb35ea0da4672e8
cary-sas <61653855+cary-sas@users.noreply.github.com>
2022-04-20 02:28:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -357,6 +357,7 @@ checkFirewalldAllowPort() {
 # 检查文件目录以及path路径
 readConfigHostPathUUID() {
 	currentPath=
+	currentDefaultPort=
 	currentUUID=
 	currentHost=
 	currentPort=
@@ -376,6 +377,16 @@ readConfigHostPathUUID() {
 		elif [[ $(echo "${fallback}" | jq -r .dest) == 31299 ]]; then
 			currentPath=$(echo "${path}" | awk -F "[v][w][s]" '{print $1}')
 		fi
+
+		local defaultPortFile=
+		defaultPortFile=$(find ${configPath}* | grep "default")
+
+		if [[ -n "${defaultPortFile}" ]]; then
+			currentDefaultPort=$(echo "${defaultPortFile}" | awk -F [_] '{print $4}')
+		else
+			currentDefaultPort=443
+		fi
+
 	fi
 	if [[ "${coreInstallType}" == "1" ]]; then
 		currentHost=$(jq -r .inbounds[0].streamSettings.xtlsSettings.certificates[0].certificateFile ${configPath}${frontingType}.json | awk -F '[t][l][s][/]' '{print $2}' | awk -F '[.][c][r][t]' '{print $1}')
@@ -808,17 +819,21 @@ EOF
 
 	else
 		cat <<EOF >${nginxConfigPath}alone.conf
+		server {
+				listen 80;
+				server_name _;
+				return 403;
+        }
+		server {
+				listen 127.0.0.1:31300;
+				server_name _;
+				return 403;
+		}
         server {
         	listen 80;
         	listen [::]:80;
         	server_name ${domain};
-        	# shellcheck disable=SC2154
-        	return 301 https://${domain}\${request_uri};
-        }
-        server {
-        		listen 127.0.0.1:31300;
-        		server_name _;
-        		return 403;
+        	return 302 https://${domain}\${request_uri};
         }
 EOF
 	fi
@@ -1109,12 +1124,14 @@ handleNginx() {
 			echoContent red " ---> 请手动尝试安装nginx后，再次执行脚本"
 			exit 0
 		fi
+		echoContent green " ---> Nginx启动成功"
 	elif [[ -n $(pgrep -f "nginx") ]] && [[ "$1" == "stop" ]]; then
 		systemctl stop nginx
 		sleep 0.5
 		if [[ -n $(pgrep -f "nginx") ]]; then
 			pgrep -f "nginx" | xargs kill -9
 		fi
+		echoContent green " ---> Nginx关闭成功"
 	fi
 }
 
@@ -1622,7 +1639,7 @@ handleXray() {
 		if [[ -n $(pgrep -f "xray/xray") ]]; then
 			echoContent green " ---> Xray启动成功"
 		else
-			echoContent red "xray启动失败"
+			echoContent red "Xray启动失败"
 			echoContent red "请手动执行【/etc/v2ray-agent/xray/xray -confdir /etc/v2ray-agent/xray/conf】，查看错误日志"
 			exit 0
 		fi
@@ -1645,7 +1662,8 @@ getClients() {
 	if [[ ${addClientsStatus} == "true" ]]; then
 		if [[ ! -f "${path}" ]]; then
 			echo
-			local protocol=$(echo "${path}" | awk -F "[_]" '{print $2 $3}')
+			local protocol
+			protocol=$(echo "${path}" | awk -F "[_]" '{print $2 $3}')
 			echoContent yellow "没有读取到此协议[${protocol}]上一次安装的配置文件，采用配置文件的第一个uuid"
 		else
 			previousClients=$(jq -r ".inbounds[0].settings.clients" "${path}")
@@ -1809,28 +1827,28 @@ EOF
 {
 "inbounds":[
     {
-  "port": 31297,
-  "listen": "127.0.0.1",
-  "protocol": "vless",
-  "tag":"VLESSWS",
-  "settings": {
-    "clients": [
-      {
-        "id": "${uuid}",
-        "email": "${domain}_VLESS_WS"
-      }
-    ],
-    "decryption": "none"
-  },
-  "streamSettings": {
-    "network": "ws",
-    "security": "none",
-    "wsSettings": {
-      "acceptProxyProtocol": true,
-      "path": "/${customPath}ws"
-    }
-  }
-}
+	  "port": 31297,
+	  "listen": "127.0.0.1",
+	  "protocol": "vless",
+	  "tag":"VLESSWS",
+	  "settings": {
+		"clients": [
+		  {
+			"id": "${uuid}",
+			"email": "${domain}_VLESS_WS"
+		  }
+		],
+		"decryption": "none"
+	  },
+	  "streamSettings": {
+		"network": "ws",
+		"security": "none",
+		"wsSettings": {
+		  "acceptProxyProtocol": true,
+		  "path": "/${customPath}ws"
+		}
+	  }
+	}
 ]
 }
 EOF
@@ -1975,7 +1993,7 @@ EOF
     "network": "tcp",
     "security": "tls",
     "tlsSettings": {
-      "minVersion": "1.2",
+      "minVersion": "1.3",
       "alpn": [
         "http/1.1",
         "h2"
@@ -2217,28 +2235,28 @@ EOF
 {
 "inbounds":[
     {
-  "port": 31297,
-  "listen": "127.0.0.1",
-  "protocol": "vless",
-  "tag":"VLESSWS",
-  "settings": {
-    "clients": [
-      {
-        "id": "${uuid}",
-        "email": "${domain}_VLESS_WS"
-      }
-    ],
-    "decryption": "none"
-  },
-  "streamSettings": {
-    "network": "ws",
-    "security": "none",
-    "wsSettings": {
-      "acceptProxyProtocol": true,
-      "path": "/${customPath}ws"
-    }
-  }
-}
+	  "port": 31297,
+	  "listen": "127.0.0.1",
+	  "protocol": "vless",
+	  "tag":"VLESSWS",
+	  "settings": {
+		"clients": [
+		  {
+			"id": "${uuid}",
+			"email": "${domain}_VLESS_WS"
+		  }
+		],
+		"decryption": "none"
+	  },
+	  "streamSettings": {
+		"network": "ws",
+		"security": "none",
+		"wsSettings": {
+		  "acceptProxyProtocol": true,
+		  "path": "/${customPath}ws"
+		}
+	  }
+	}
 ]
 }
 EOF
@@ -2382,7 +2400,7 @@ EOF
     "network": "tcp",
     "security": "xtls",
     "xtlsSettings": {
-      "minVersion": "1.2",
+      "minVersion": "1.3",
       "alpn": [
         "http/1.1",
         "h2"
@@ -2480,11 +2498,13 @@ defaultBase64Code() {
 	local port=
 	if echo "${hostPort}" | grep -q ":"; then
 		host=$(echo "${hostPort}" | awk -F "[:]" '{print $1}')
-		port=$(echo "${hostPort}" | awk -F "[:]" '{print $2}')
+		# port=$(echo "${hostPort}" | awk -F "[:]" '{print $2}')
 	else
 		host=${hostPort}
-		port=443
+		# port=443
 	fi
+
+	port=${currentDefaultPort}
 
 	local path=$5
 	local add=$6
@@ -2742,7 +2762,62 @@ showAccounts() {
 		echoContent red " ---> 未安装"
 	fi
 }
+# 移除nginx302配置
+removeNginx302() {
+	# 查找到302那行并删除
+	local line302Result=
+	line302Result=$(grep -n "return 302" </etc/nginx/conf.d/alone.conf | tail -n 1)
 
+	if ! echo "${line302Result}" | grep -q "request_uri"; then
+		sed -i "$(echo "${line302Result}" | awk -F "[:]" '{print $1}')d" /etc/nginx/conf.d/alone.conf
+		echoContent green " ---> 移除302重定向成功"
+		exit 0
+	fi
+
+}
+
+# 检查302是否成功
+checkNginx302() {
+	local domain302Status=
+	domain302Status=$(curl -s "https://${currentHost}")
+	if echo "${domain302Status}" | grep -q "302"; then
+		local domain302Result=
+		domain302Result=$(curl -L -s "https://${currentHost}")
+		if [[ -n "${domain302Result}" ]]; then
+			echoContent green " ---> 302重定向设置成功"
+			exit 0
+		fi
+	fi
+	echoContent red " ---> 302重定向设置失败，请仔细检查是否和示例相同"
+	backupNginxConfig restoreBackup
+}
+
+# 备份恢复nginx文件
+backupNginxConfig() {
+	if [[ "$1" == "backup" ]]; then
+		cp /etc/nginx/conf.d/alone.conf /etc/v2ray-agent/alone_backup.conf
+		echoContent green " ---> nginx配置文件备份成功"
+	fi
+
+	if [[ "$1" == "restoreBackup" ]] && [[ -f "/etc/v2ray-agent/alone_backup.conf" ]]; then
+		cp /etc/v2ray-agent/alone_backup.conf /etc/nginx/conf.d/alone.conf
+		echoContent green " ---> nginx配置文件恢复备份成功"
+		rm /etc/v2ray-agent/alone_backup.conf
+	fi
+
+}
+# 添加302配置
+addNginx302() {
+	local line302Result=
+	line302Result=$(grep -n "Strict-Transport-Security" </etc/nginx/conf.d/alone.conf | tail -n 1)
+
+	if [[ -n "${line302Result}" ]]; then
+		sed "$(echo "${line302Result}" | awk -F "[:]" '{print $1}')i return 302 '$1';" /etc/nginx/conf.d/alone.conf >/etc/nginx/conf.d/tmpfile && mv /etc/nginx/conf.d/tmpfile /etc/nginx/conf.d/alone.conf
+	else
+		echoContent red " ---> 302添加失败"
+		backupNginxConfig restoreBackup
+	fi
+}
 # 更新伪装站
 updateNginxBlog() {
 	echoContent skyBlue "\n进度 $1/${totalProgress} : 更换伪装站点"
@@ -2757,11 +2832,41 @@ updateNginxBlog() {
 	echoContent yellow "7.企业站02"
 	echoContent yellow "8.个人博客02"
 	echoContent yellow "9.404自动跳转baidu"
+	echoContent yellow "10.302重定向网站"
 	echoContent red "=============================================================="
 	read -r -p "请选择:" selectInstallNginxBlogType
 
+	if [[ "${selectInstallNginxBlogType}" == "10" ]]; then
+		echoContent red "\n=============================================================="
+		echoContent yellow "重定向的优先级更高，配置302之后如果更改伪装站点，根路由下伪装站点将不起作用"
+		echoContent yellow "如想要伪装站点实现作用需删除302重定向配置\n"
+		echoContent yellow "1.添加"
+		echoContent yellow "2.删除"
+		echoContent red "=============================================================="
+		read -r -p "请选择:" redirectStatus
+
+		if [[ "${redirectStatus}" == "1" ]]; then
+			backupNginxConfig backup
+			read -r -p "请输入要重定向的域名,例如 https://www.baidu.com:" redirectDomain
+			removeNginx302
+			addNginx302 "${redirectDomain}"
+			handleNginx stop
+			handleNginx start
+			if [[ -z $(pgrep -f nginx) ]]; then
+				backupNginxConfig restoreBackup
+				handleNginx start
+				exit 0
+			fi
+			checkNginx302
+			exit 0
+		fi
+		if [[ "${redirectStatus}" == "2" ]]; then
+			removeNginx302
+			echoContent green " ---> 移除302重定向成功"
+			exit 0
+		fi
+	fi
 	if [[ "${selectInstallNginxBlogType}" =~ ^[1-9]$ ]]; then
-		#		rm -rf /usr/share/nginx/html
 		rm -rf /usr/share/nginx/*
 		if wget --help | grep -q show-progress; then
 			wget -c -q --show-progress -P /usr/share/nginx "https://raw.githubusercontent.com/cary-sas/v2ray-agent/master/fodder/blog/unable/html${selectInstallNginxBlogType}.zip" >/dev/null
@@ -2795,24 +2900,39 @@ addCorePort() {
 	read -r -p "请选择:" selectNewPortType
 	if [[ "${selectNewPortType}" == "1" ]]; then
 		read -r -p "请输入端口号:" newPort
+		read -r -p "请输入默认的端口号，同时会更改订阅端口以及节点端口，[回车]默认443:" defaultPort
+
+		if [[ -n "${defaultPort}" ]]; then
+			rm -rf "$(find ${configPath}* | grep "default")"
+		fi
+
 		if [[ -n "${newPort}" ]]; then
 
 			while read -r port; do
-				cat <<EOF >"${configPath}02_dokodemodoor_inbounds_${port}.json"
+				rm -rf "$(find ${configPath}* | grep "${port}")"
+
+				local fileName=
+				if [[ -n "${defaultPort}" && "${port}" == "${defaultPort}" ]]; then
+					fileName="${configPath}02_dokodemodoor_inbounds_${port}_default.json"
+				else
+					fileName="${configPath}02_dokodemodoor_inbounds_${port}.json"
+				fi
+
+				cat <<EOF >"${fileName}"
 {
   "inbounds": [
-    {
-      "listen": "0.0.0.0",
-      "port": ${port},
-      "protocol": "dokodemo-door",
-      "settings": {
-        "address": "127.0.0.1",
-        "port": 443,
-        "network": "tcp",
-        "followRedirect": false
-      },
-      "tag": "dokodemo-door-newPort-${port}"
-    }
+	{
+	  "listen": "0.0.0.0",
+	  "port": ${port},
+	  "protocol": "dokodemo-door",
+	  "settings": {
+		"address": "127.0.0.1",
+		"port": 443,
+		"network": "tcp",
+		"followRedirect": false
+	  },
+	  "tag": "dokodemo-door-newPort-${port}"
+	}
   ]
 }
 EOF
@@ -2851,16 +2971,24 @@ unInstall() {
 		echoContent green " ---> 停止Nginx成功"
 	fi
 
-	handleV2Ray stop
-	#	handleTrojanGo stop
+	if [[ "${coreInstallType}" == "1" ]]; then
+		handleXray stop
+		rm -rf /etc/systemd/system/xray.service
+		echoContent green " ---> 删除Xray开机自启完成"
+
+	elif [[ "${coreInstallType}" == "2" ]]; then
+
+		handleV2Ray stop
+		rm -rf /etc/systemd/system/v2ray.service
+		echoContent green " ---> 删除V2Ray开机自启完成"
+
+	fi
 
 	if [[ -f "/root/.acme.sh/acme.sh.env" ]] && grep -q 'acme.sh.env' </root/.bashrc; then
 		sed -i 's/. "\/root\/.acme.sh\/acme.sh.env"//g' "$(grep '. "/root/.acme.sh/acme.sh.env"' -rl /root/.bashrc)"
 	fi
 	rm -rf /root/.acme.sh
 	echoContent green " ---> 删除acme.sh完成"
-	rm -rf /etc/systemd/system/v2ray.service
-	echoContent green " ---> 删除V2Ray开机自启完成"
 
 	rm -rf /tmp/v2ray-agent-tls/*
 	if [[ -d "/etc/v2ray-agent/tls" ]] && [[ -n $(find /etc/v2ray-agent/tls/ -name "*.key") ]] && [[ -n $(find /etc/v2ray-agent/tls/ -name "*.crt") ]]; then
@@ -2872,6 +3000,12 @@ unInstall() {
 
 	rm -rf /etc/v2ray-agent
 	rm -rf ${nginxConfigPath}alone.conf
+
+	if [[ -d "/usr/share/nginx/html" && -f "/usr/share/nginx/html/check" ]]; then
+		rm -rf /usr/share/nginx/html
+		echoContent green " ---> 删除伪装网站完成"
+	fi
+
 	rm -rf /usr/bin/vasma
 	rm -rf /usr/sbin/vasma
 	echoContent green " ---> 卸载快捷方式完成"
@@ -2977,7 +3111,7 @@ customUserEmail() {
 	if [[ -z "${currentCustomEmail}" ]]; then
 		currentCustomEmail="${currentHost}_${currentCustomUUID}"
 		echoContent yellow "email: ${currentCustomEmail}\n"
-#		echoContent red " ---> email不可为空"
+		#		echoContent red " ---> email不可为空"
 	else
 		jq -r -c '.inbounds[0].settings.clients[].email' ${configPath}${frontingType}.json | while read -r line; do
 			if [[ "${line}" == "${currentCustomEmail}" ]]; then
@@ -4352,9 +4486,15 @@ subscribe() {
 				echo "${base64Result}" >"/etc/v2ray-agent/subscribe/${email}"
 				echoContent skyBlue "--------------------------------------------------------------"
 				echoContent yellow "email:$(echo "${email}" | awk -F "[_]" '{print $1}')\n"
-				echoContent yellow "url:https://${currentHost}/s/${email}\n"
-				echoContent yellow "在线二维码:https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=https://${currentHost}/s/${email}\n"
-				echo "https://${currentHost}/s/${email}" | qrencode -s 10 -m 1 -t UTF8
+				local currentDomain=${currentHost}
+
+				if [[ -n "${currentDefaultPort}" && "${currentDefaultPort}" != "443" ]]; then
+					currentDomain="${currentHost}:${currentDefaultPort}"
+				fi
+
+				echoContent yellow "url:https://${currentDomain}/s/${email}\n"
+				echoContent yellow "在线二维码:https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=https://${currentDomain}/s/${email}\n"
+				echo "https://${currentDomain}/s/${email}" | qrencode -s 10 -m 1 -t UTF8
 				echoContent skyBlue "--------------------------------------------------------------"
 			done
 		fi
@@ -4410,7 +4550,7 @@ menu() {
 	cd "$HOME" || exit
 	echoContent red "\n=============================================================="
 	echoContent green "作者:cary-sas"
-	echoContent green "当前版本:v2.5.55"
+	echoContent green "当前版本:v2.5.59"
 	echoContent green "Github:https://github.com/cary-sas/v2ray-agent"
 	echoContent green "描述:八合一共存脚本\c"
 	showInstallStatus
